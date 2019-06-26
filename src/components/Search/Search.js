0082cc251c37221040a2ad205cccb1dd2cbaa6df
Mike Bell <michaelbell.va@gmail.com>
2019-06-26 14:24:34 -0400

    shorten search field names

--- src/components/Search/Search.js
+++ src/components/Search/Search.js
@@ -39,31 +39,31 @@ class Search extends Component {
             <div className="row">
               <div className="input-field col s6">
                 <input id="caseNameInput" type="text" value={this.state.caseNameInput} onChange={(e) => this.setState({ caseNameInput: e.target.value })} />
-                <label htmlFor="caseNameInput">Search by Case Name</label>
+                <label htmlFor="caseNameInput">Case Name</label>
               </div>
               <div className="input-field col s6">
                 <input id="citationInput" type="text" value={this.state.citationInput} onChange={(e) => this.setState({ citationInput: e.target.value })} />
-                <label htmlFor="citationInput">Search by Citation</label>
+                <label htmlFor="citationInput">Citation</label>
               </div>
             </div>
             <div className="row">
               <div className="input-field col s6">
                 <input id="courtInput" type="text" value={this.state.courtInput} onChange={(e) => this.setState({ courtInput: e.target.value })} />
-                <label htmlFor="courtInput">Search by Court</label>
+                <label htmlFor="courtInput">Search Court</label>
               </div>
               <div className="input-field col s6">
                 <input id="keywordsInput" type="text" value={this.state.keywordsInput} onChange={(e) => this.setState({ keywordsInput: e.target.value })} />
-                <label htmlFor="keywordsInput">Search by Keyword(s)</label>
+                <label htmlFor="keywordsInput">Search Keyword(s)</label>
               </div>
             </div>
             <div className="row">
               <div className="input-field col s6">
                 <input id="judgesInput" type="text" value={this.state.judgesInput} onChange={(e) => this.setState({ judgesInput: e.target.value })} />
-                <label htmlFor="judgesInput">Search by Judge(s)</label>
+                <label htmlFor="judgesInput">Search Judge(s)</label>
               </div>
               <div className="input-field col s6">
                 <input id="docTypeInput" type="text" value={this.state.docTypeInput} onChange={(e) => this.setState({ docTypeInput: e.target.value })} />
-                <label htmlFor="docTypeInput">Search by Document Type</label>
+                <label htmlFor="docTypeInput">Search Document Type</label>
               </div>
             </div>
             <button type="submit" name="action" className="waves-effect waves-light btn-large right">
