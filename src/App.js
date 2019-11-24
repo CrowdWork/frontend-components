ce705c33e0aad236563874b13fc529afb1c5ea4a
Mike Bell <michaelbell.va@gmail.com>
2019-11-24 18:59:56 -0500

    redeploying

--- src/App.js
+++ src/App.js
@@ -68,8 +68,8 @@ class App extends Component {
     subjects: [],
     subjectSelected: '',
     topic: '',
-    url: "http://localhost:4000",
-    // url: "https://ble-backend.herokuapp.com",
+    // url: "http://localhost:4000",
+    url: "https://ble-backend.herokuapp.com",
     userCount: null,
     users: []
   }
