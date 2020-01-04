60e649d9ddc2c7fdb968ebb9aa3106e5b35fc0cf
Mike Bell <michaelbell.va@gmail.com>
2020-01-04 18:55:33 -0500

    point to heroku

--- src/App.js
+++ src/App.js
@@ -71,8 +71,8 @@ class App extends Component {
     subjects: [],
     subjectSelected: '',
     topic: '',
-    // url: "http://localhost:4000",
-    url: "https://ble-backend.herokuapp.com",
+    url: "http://localhost:4000",
+    // url: "https://ble-backend.herokuapp.com",
     userCount: null,
     users: [] 
   }
@@ -168,7 +168,7 @@ class App extends Component {
   // adding a function that picks a specific subject
   subjectSelection = (value) => {
     try {
-      this.setState(() => { 
+      this.setState(() => {
         return {
           subjectSelected: value
         }
